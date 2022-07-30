9bb4c5845054c2b5b26cd3de4adc615a46cb4599
baolhq <quocbao.kaiser@gmail.com>
2022-07-30 16:04:03 +0700

    heroku setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: node(),
+  },
 };
 
 export default config;
